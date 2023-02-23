3a9527bec43e6e685b4516a7e59b351c04453131
jaiswalsachin71 <104354479+jaiswalsachin71@users.noreply.github.com>
2023-02-23 16:14:11 +0530

    Update Sachin.java
    Sachin updates

--- javaGitpro/src/javaGitpro/Sachin.java
+++ javaGitpro/src/javaGitpro/Sachin.java
@@ -3,5 +3,12 @@ package javaGitpro;
 public class Sachin {
 public static void main(String[] args) {
 	System.out.println("class sachin");
+	Sachin s=new Saching();
+	s.m1();
 }
+	public void m1()
+	{
+	System.out.println("method m1");
+	}
+	
 }
